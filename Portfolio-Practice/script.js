adbc660a1f42d0c71c9a224866a9127ef7902a6e
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-03-27 17:35:32 +0530

    I have completed the portfolio Project Now All the project are in working condition.

--- Portfolio-Practice/script.js
+++ Portfolio-Practice/script.js
@@ -3,3 +3,11 @@
   dayNight.addEventListener("click",()=>{
     banner.classList.toggle("night");
   })
+
+  let typingEffect = new Typed("#text",{
+    strings:["Mayur Pratap Singh","a B.tech Graduate","from Galgotias University"],
+    loop:true,
+    typeSpeed:80,
+    backSpeed:50,
+    backDelay:400
+  })
