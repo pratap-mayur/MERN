537e96f00e97f8fc3e10d96703260345df6ddbdb
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-04-02 18:12:16 +0530

    i have created a tailwind project mostly a introduction card

---
+++ TAILWINDTUT/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],  // Scan dist folder for HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
