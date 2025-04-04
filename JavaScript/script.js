cb5f917f3fa372812bdffc66727f5f3572e444c5
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-04-05 00:07:42 +0530

    lets go for javascript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,8 @@
-console.log("yiiiii")
+let ns = "john";
+console.log(ns);
+str = "Hello";
+str2 = "world";
+console.log("str and str2 have valuer",str,str2);
+console.log("str and str2 have valuer "+ str+" "+ str2);
+console.log(`str and str2 have valuer ${str} ${str2}`)
+console.log("str and str2 have valuer")
