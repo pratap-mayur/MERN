279e05152ede8ce838654df136cab6ee722403df
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-04-06 22:06:21 +0530

    lets go for javascript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,12 @@ console.log("str and str2 have valuer",str,str2);
 console.log("str and str2 have valuer "+ str+" "+ str2);
 console.log(`str and str2 have valuer ${str} ${str2}`)
 console.log("str and str2 have valuer")
+
+function abcd(a,b,c){
+    console.log(a);
+
+}
+abcd(12)
+
+var arr= [1,2,3,4,5,6,7];
+arr.shift(4)
