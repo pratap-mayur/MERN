e51abc9ddfc48900949cc988859ae83648de8f4d
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-04-08 23:22:49 +0530

    add all details in a single book

--- learn_react/01/src/Mobile.js
+++ learn_react/01/src/Mobile.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import MobileList from './MobileList';
 
+const book = [{
+     image : 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/q/n/s/-original-imah4jyfrgsbebg9.jpeg?q=70',
+ title : 'iPhone',
+ price : '105000'
+}
+
+,{
+     image : 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/t/8/p/-original-imah4jykf9e4a244.jpeg?q=70',
+ title : 'iPhone2',
+ price : '105555'
+}]
+
 export default function Mobile() {
   return (
-    <div>
-      <MobileList />
-      <MobileList />
+    <div>{
+        book.map((ele)=>{
+      return <MobileList  image={ele.image} title={ele.title} price={ele.price}/>
+        })}
     </div>
   );
 }
--- learn_react/01/src/MobileList.js
+++ learn_react/01/src/MobileList.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './MobileList.css';
 
-export default function MobileList() {
+export default function MobileList(props) {
+    const {image,title,price} = props;
+
   return (
     <div className='main'>
-      <img
-        src='https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/q/n/s/-original-imah4jyfrgsbebg9.jpeg?q=70'
+      <img className='image'
+        src={image}
         alt='iPhone'
       />
-      <h2>iPhone</h2>
-      <p>rs 105000</p>
+      <div>
+      <h2>{title}</h2>
+      <p>rs {price}</p>
       <button>Add to cart</button>
+      </div>
     </div>
   );
 }
