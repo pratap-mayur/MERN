8f8b0d8aee73269747094e0f6ee98e1acff78e52
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-04-14 23:55:25 +0530

    Learning modules in node

--- Backend-Projects/Node/index.js
+++ Backend-Projects/Node/index.js
@@ -1,4 +1,9 @@
-function hello(){
-    console.log("Hello World");
-}
-hello();
+// function hello(){
+//     console.log("Hello World");
+// }
+// hello();
+
+require("./add");
+const sum = add(5,7);
+
+console.log(sum);
