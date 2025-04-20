17108697f8fcac1bd60fbd620aedafa772774216
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2025-04-20 17:08:33 +0530

    created components such as Css for home, Components, time-chedule

--- FullStack-Projects/Book-My-Show/front-end/src/Components/TimeSchedule.js
+++ FullStack-Projects/Book-My-Show/front-end/src/Components/TimeSchedule.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import RadioComponents from './RadioComponents'
+import { slots } from '../Mlist';
+import "../CSS/TimeSchedule.css";
 
 const TimeSchedule = () => {
   return (
-    <div>
-      
+    <div className='Slot_container'>
+        <h1 className='TS_heading'>Select a Schedule</h1>
+      <div className='TS_main_container'>
+        {slots.map((el,index) => {
+            return (
+                <RadioComponents text={el} key={index}/>
+            )
+        })}
+      </div>
     </div>
   )
 }
--- FullStack-Projects/Book-My-Show/front-end/src/Mlist.js
+++ FullStack-Projects/Book-My-Show/front-end/src/Mlist.js
@@ -3,3 +3,4 @@ let slots = ['10:00 AM','01:00 PM','04:00 PM', '06:00 PM']
 
 
 exports.moviesList = moviesList;
+exports.slots = slots;
